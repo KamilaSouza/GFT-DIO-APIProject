8add78190db625c8306695dc17387b8d14ee0a4e
Kamila <kamisouzas@gmail.com>
2022-05-13 16:21:11 -0300

    Model

---
+++ src/main/java/dio/challenge/designpatterns/model/ClientRepository.java
@@ -0,0 +1,8 @@
+package dio.challenge.designpatterns.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository  //optional
+public interface ClientRepository extends CrudRepository <Client, Long> {
+}
